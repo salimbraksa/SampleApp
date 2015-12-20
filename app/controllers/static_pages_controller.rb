d72ab09a3452230d2b3005c8f346317b4d9fe5c4
salimbraksa <salim.braksa@gmail.com>
2015-12-20 01:50:53 +0000

    Created static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,12 @@
 class StaticPagesController < ApplicationController
-	
-  def home
-  end
 
-  def help
-  end
+  	def home
+  	end
 
-  def about
-  end
+  	def help
+  	end
+
+  	def about
+  	end
 
 end
